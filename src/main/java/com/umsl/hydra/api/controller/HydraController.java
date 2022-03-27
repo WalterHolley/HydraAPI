8d5e50806432fe13475aca9a5b6e5648159d8792
Walter Holley III <holley.walter@gmail.com>
2022-03-27 16:50:56 -0500

    Controller Setup
    -setup for initial controller request

--- src/main/java/com/umsl/hydra/api/controller/HydraController.java
+++ src/main/java/com/umsl/hydra/api/controller/HydraController.java
@@ -1,4 +1,13 @@
 package com.umsl.hydra.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HydraController {
+
+    @GetMapping("/start")
+    public String startGame(){
+        return String.format("Thanks for playing asshole");
+    }
 }
