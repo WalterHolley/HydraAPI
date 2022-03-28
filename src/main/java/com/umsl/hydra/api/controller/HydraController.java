1ca820d1612e894b0c8ce0612a711c110dfe8b8d
Walter Holley III <holley.walter@gmail.com>
2022-03-27 22:12:15 -0500

    Controller Setup
    -setup for initial controller request

--- src/main/java/com/umsl/hydra/api/controller/HydraController.java
+++ src/main/java/com/umsl/hydra/api/controller/HydraController.java
@@ -1,13 +1,17 @@
 package com.umsl.hydra.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 @RestController
 public class HydraController {
 
-    @GetMapping("/start")
-    public String startGame(){
-        return String.format("Thanks for playing asshole");
+    @CrossOrigin(origins = "http://localhost:8081", allowedHeaders = "*")
+    @RequestMapping(value = "/start", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public String startGame(@RequestParam(required = false, defaultValue = "Sure") String id){
+        return String.format("{ \"text\": \"Thanks for playing asshole\"}");
     }
 }
