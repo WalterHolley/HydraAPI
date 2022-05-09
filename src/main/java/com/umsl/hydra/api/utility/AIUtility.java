5e831190bfed0639f82901cd068eb0781f5d1ba2
ndyer115 <ndyer115@gmail.com>
2022-05-09 12:11:53 -0500

    fixed issue with hard difficulty pattern generator and all unit tests are written

--- src/main/java/com/umsl/hydra/api/utility/AIUtility.java
+++ src/main/java/com/umsl/hydra/api/utility/AIUtility.java
@@ -15,7 +15,7 @@ public class AIUtility {
         return result;
     }
 
-    private int[] getEnemyPattern(DifficultyEnum difficulty, int[] playerPattern){
+    public int[] getEnemyPattern(DifficultyEnum difficulty, int[] playerPattern){
         int[] enemyPattern;
 
         switch(difficulty){
@@ -35,16 +35,16 @@ public class AIUtility {
      * @param playerPattern the pattern provided by the player
      * @return array of the most likely pattern to win against the player's selection
      */
-    private int[] getHardPattern(int[] playerPattern){
+    public int[] getHardPattern(int[] playerPattern){
         int[] enemyPattern = playerPattern;
         int swap;
-        enemyPattern[2] = enemyPattern[1];
         if(enemyPattern[1] == 0)
             swap = 1;
         else
             swap = 0;
         enemyPattern[0] = swap;
-
+        enemyPattern[2] = enemyPattern[1];
+        enemyPattern[1] = enemyPattern[0];
         return enemyPattern;
 
     }
@@ -53,7 +53,7 @@ public class AIUtility {
      * Gets a random 3 integer(1 or 0) pattern
      * @return
      */
-    private int[] getRandomPattern(){
+    public int[] getRandomPattern(){
         int[] gamePattern = new int[3];
         gamePattern[0] = (int) Math.floor(Math.random() * 2);
         gamePattern[1] = (int) Math.floor(Math.random() * 2);
@@ -68,7 +68,7 @@ public class AIUtility {
      * @param enemyPattern The enemy's pattern
      * @return true if the player pattern wins, otherwise false.
      */
-    private boolean executeRound(int[] playerPattern, int[] enemyPattern){
+    public boolean executeRound(int[] playerPattern, int[] enemyPattern){
         boolean isRunning = true;
         boolean result = false;
 
@@ -102,7 +102,7 @@ public class AIUtility {
      * @param pattern2
      * @return true if both patterns match.  otherwise false
      */
-    private boolean patternsMatch(int[] pattern1, int[] pattern2){
+    public boolean patternsMatch(int[] pattern1, int[] pattern2){
         boolean result = true;
         if(pattern1.length != pattern2.length)
             result = false;
--- src/test/java/com/umsl/hydra/api/utility/AIUtilityTest.java
+++ src/test/java/com/umsl/hydra/api/utility/AIUtilityTest.java
@@ -16,7 +16,7 @@ public class AIUtilityTest {
     private AIUtility utility;
 
 @Test
-public void AIUTilSuccess(){
+public void wonRoundSuccess(){
     short i = 0;
     boolean winner;
     utility = new AIUtility();
@@ -26,8 +26,60 @@ public void AIUTilSuccess(){
         i = 1;
 
     assertNotEquals(0,i );
-
-
 }
+@Test
+public void getEnemyPatternSuccess(){
+    short i = 0;
+    int[] pattern;
+    utility = new AIUtility();
+    pattern = utility.getEnemyPattern(DifficultyEnum.EASY, new int[] {1, 0, 1});
+    if ((pattern[0] == 0 || pattern[0] == 1) && (pattern[1] == 0 || pattern[1] == 1) && (pattern[2] == 0 || pattern[2] == 1))
+        i = 1;
+    assertNotEquals(0,i);
+}
+@Test
+public void getHardPatternSuccess(){
+    int[] playerpattern = {1, 0 , 1};
+    int[] testpattern = {1, 1, 0};
+    int[] AIpattern;
+    utility = new AIUtility();
+    AIpattern = utility.getHardPattern(playerpattern);
+    assertArrayEquals(testpattern, AIpattern);
+}
+@Test
+    public void getRandomPatternSuccess(){
+    short i = 0;
+    int[] pattern;
+    utility = new AIUtility();
+    pattern = utility.getRandomPattern();
+    if ((pattern[0] == 0 || pattern[0] == 1) && (pattern[1] == 0 || pattern[1] == 1) && (pattern[2] == 0 || pattern[2] == 1))
+        i = 1;
+    assertNotEquals(0,i);
+}
+@Test
+    public void executeRoundSuccess(){
+    int[] player = {1, 0, 1};
+    int[] AI = {1, 0, 0};
+    boolean check;
+    short i = 0;
+    utility = new AIUtility();
+    check = utility.executeRound(player, AI);
+    if(check || !check)
+        i = 1;
+    assertNotEquals(0, i);
+}
+@Test
+    public void patternsMatchSuccess(){
+    int[] first = {1, 0, 0};
+    int[] second = {1, 0, 1};
+    boolean check;
+    utility = new AIUtility();
+    check = utility.patternsMatch(first, second);
+    assertEquals(false,check);
 
+    int[] first2 = {1, 0, 0};
+    int[] second2 = {1, 0, 0};
+    check = utility.patternsMatch(first2, second2);
+    assertEquals(true,check);
+}
 }
